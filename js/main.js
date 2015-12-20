291eb476245759b525ee8a6b632800d98fb80dfa
Felipe Canales <f.canales.27@gmail.com>
2015-12-20 14:43:40 -0300

    App taller de sia

---
+++ js/main.js
@@ -0,0 +1,51 @@
+/**
+ * Created by fcanales on 20-12-15.
+ */
+
+function banio_on() {
+    $(document).ready(function () {
+        $("#banio_on").click(function () {
+            $("#casa").html('<img src="images/casa_banio.gif" alt="">')
+        })
+    })
+}
+
+function banio_off() {
+    $(document).ready(function () {
+        $("#banio_off").click(function () {
+            $("#casa").html('<img src="images/casa.gif" alt="">')
+        })
+    })
+}
+
+function main_on() {
+    $(document).ready(function () {
+        $("#main_on").click(function () {
+            $("#casa").html('<img src="images/casa_primaria.gif" alt="">')
+        })
+    })
+}
+
+function main_off() {
+    $(document).ready(function () {
+        $("#main_off").click(function () {
+            $("#casa").html('<img src="images/casa.gif" alt="">')
+        })
+    })
+}
+
+function cocina_on() {
+    $(document).ready(function () {
+        $("#cocina_on").click(function () {
+            $("#casa").html('<img src="images/casa_cocina.gif" alt="">')
+        })
+    })
+}
+
+function cocina_off() {
+    $(document).ready(function () {
+        $("#cocina_off").click(function () {
+            $("#casa").html('<img src="images/casa.gif" alt="">')
+        })
+    })
+}
